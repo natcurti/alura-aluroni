8286017110bd91c28956c957415d817be64dc619
natcurti <naticurti01@gmail.com>
2024-02-26 14:51:32 -0300

    Initialize project Aluroni

---
+++ src/pages/Menu/index.tsx
@@ -0,0 +1,11 @@
+import styles from "./Menu.module.scss";
+
+const Menu = () => {
+  return (
+    <main>
+      <nav className={styles.menu}>Aluroni</nav>
+    </main>
+  );
+};
+
+export default Menu;
