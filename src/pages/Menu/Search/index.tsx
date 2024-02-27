c39f78117dc2b45891f3830a202d89eaef20a255
natcurti <naticurti01@gmail.com>
2024-02-27 09:02:07 -0300

    Handle filter state and add classnames lib

---
+++ src/pages/Menu/Search/index.tsx
@@ -0,0 +1,18 @@
+import styles from "./Search.module.scss";
+import { CgSearch } from "react-icons/cg";
+
+interface ISearch {
+  search: string;
+  setSearch: React.Dispatch<React.SetStateAction<string>>;
+}
+
+const Search = ({ search, setSearch }: ISearch) => {
+  return (
+    <div className={styles.search}>
+      <input value={search} onChange={(e) => setSearch(e.target.value)} />
+      <CgSearch size={20} color="#4C4D5E" />
+    </div>
+  );
+};
+
+export default Search;
--- src/pages/Menu/index.tsx
+++ src/pages/Menu/index.tsx
@@ -1,7 +1,14 @@
 import styles from "./Menu.module.scss";
 import { ReactComponent as Logo } from "assets/logo.svg";
+import Search from "./Search";
+import { useState } from "react";
+import Filters from "pages/Filters";
 
 const Menu = () => {
+  const [search, setSearch] = useState("");
+
+  const [filter, setFilter] = useState<number | null>(null);
+
   return (
     <main>
       <nav className={styles.menu}>
@@ -10,6 +17,13 @@ const Menu = () => {
       <header className={styles.header}>
         <div className={styles.header__text}>A casa do código e da massa</div>
       </header>
+      <section className={styles.menu}>
+        <h3 className={styles.menu__title}>Cardápio</h3>
+        <Search search={search} setSearch={setSearch} />
+        <div className={styles.menu__filters}>
+          <Filters filter={filter} setFilter={setFilter} />
+        </div>
+      </section>
     </main>
   );
 };
